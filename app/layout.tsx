79c1622bd474cafbf972c9f0aff873383bac7ffb
kylej21 <you@example.com>
2023-12-06 17:54:23 -0800

    customized ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import '@radix-ui/themes/styles.css';
+import './theme-config.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-const inter = Inter({ subsets: ['latin'] })
-import { Theme } from '@radix-ui/themes';
+const inter = Inter({ subsets: ['latin'],
+variable: '--font-inter' 
+})
+
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
 
 export const metadata: Metadata = {
@@ -19,8 +23,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+      <Theme appearance="light" accentColor="teal" scaling="110%">
           <NavBar/>
           <main className="p-5">
             {children}
