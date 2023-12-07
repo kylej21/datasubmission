27f8ece2f1217e560aafa885d9b4876f59af4885
kylej21 <you@example.com>
2023-12-07 10:31:15 -0800

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'],
 variable: '--font-inter' 
 })
